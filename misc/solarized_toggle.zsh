0a875876ff407ba9bb44b813b9e67bb3825708c7
Jacob.Niehus@gmail.com <Jacob.Niehus@gmail.com@b819179b-967b-96dc-1cd5-44331c136a0c>
2014-07-14 20:44:12 +0000

    Set TERM before running xtermcontrol in solarized_toggle

--- misc/solarized_toggle.zsh
+++ misc/solarized_toggle.zsh
@@ -3,13 +3,13 @@ if [[ -z "$SOLARIZED_TOGGLE" ]]; then
 fi
 
 if [ $SOLARIZED_TOGGLE = "1" ]; then
-  xtermcontrol --bg='#012833' --fg='#839496' --mouse-bg='#586e75' \
-    --mouse-fg='#93a1a1' --highlight='#000000'
+  TERM=xterm-256color xtermcontrol --bg='#012833' --fg='#839496' \
+    --mouse-bg='#586e75' --mouse-fg='#93a1a1' --highlight='#000000'
   export SOLARIZED_TOGGLE=0
   >~/.vim/after/plugin/bg.vim <<< 'set background=dark'
 else
-  xtermcontrol --bg='#fdf6e3' --fg='#657b83' --mouse-bg='#93a1a1' \
-    --mouse-fg='#586e75' --highlight='#000000'
+  TERM=xterm-256color xtermcontrol --bg='#fdf6e3' --fg='#657b83' \
+    --mouse-bg='#93a1a1' --mouse-fg='#586e75' --highlight='#000000'
   export SOLARIZED_TOGGLE=1
   >~/.vim/after/plugin/bg.vim <<< 'set background=light'
 fi
