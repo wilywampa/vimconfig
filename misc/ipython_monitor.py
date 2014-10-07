7c3b034a97ca8e04264fcce0c0a9f26fad77806b
Jacob.Niehus <Jacob.Niehus@b819179b-967b-96dc-1cd5-44331c136a0c>
2014-10-07 01:30:47 +0000

    Add syntax highlighting to ipython_monitor and turn off subchannel monitoring in vim-ipython

--- misc/ipython_monitor.py
+++ misc/ipython_monitor.py
@@ -1,6 +1,9 @@
 from IPython.kernel import KernelManager
 from IPython.kernel import find_connection_file
 import sys
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import TerminalFormatter
 
 colors = {
     'red':      '31',
@@ -24,22 +27,32 @@ km.load_connection_file()
 kc = km.client()
 kc.start_channels()
 
+if len(sys.argv) > 1:
+    term = open(sys.argv[1], 'w+')
+    sys.stdout = term
+
+lexer = PythonLexer()
+formatter = TerminalFormatter()
+
+print_idle = False
 socket = km.connect_iopub()
 while socket.recv():
     kc.iopub_channel.flush()
     msgs = kc.iopub_channel.get_msgs()
     for msg in msgs:
         if msg['msg_type'] == 'pyin':
-            prompt = ''.join('In  [%d]:' % msg['content']['execution_count'])
+            prompt = ''.join('In [%d]: ' % msg['content']['execution_count'])
             dots = '.' * len(prompt.rstrip()) + ' '
-            prompt = '\n\033[' + colors['blue'] + 'm' + prompt + '\033[0m'
+            prompt = '\n\033[' + colors['green'] + 'm' + prompt + '\033[0m'
             sys.stdout.write(prompt)
-            output = msg['content']['code'].rstrip().replace('\n', '\n' + dots)
-            sys.stdout.write(''.join(' %s' % output))
+            code = highlight(msg['content']['code'], lexer, formatter)
+            output = code.rstrip().replace('\n', '\n' + dots)
+            sys.stdout.write(output + '\n')
+            print_idle = True
         elif msg['msg_type'] == 'pyout':
-            sys.stdout.write(''.join(('\033[', colors['red'], 'm', 'Out [%d]:'
+            sys.stdout.write(''.join(('\n\033[', colors['red'], 'm', 'Out [%d]:'
                                       % msg['content']['execution_count'],
-                                      '\033[m %s\n' %
+                                      '\n\033[m%s' %
                                       msg['content']['data']['text/plain'])))
         elif msg['msg_type'] == 'pyerr':
             for line in msg['content']['traceback']:
@@ -48,6 +61,12 @@ while socket.recv():
             sys.stdout.write('\n' + msg['content']['data'])
         elif msg['msg_type'] == 'shutdown_reply':
             sys.exit(0)
+        elif (msg['msg_type'] == 'status'
+              and msg['content']['execution_state'] == 'idle'
+              and print_idle):
+            sys.stdout.write('\n' + 'idle')
+            print_idle = False
         elif not msg['msg_type'] == 'status':
             print 'msg_type = %s' % str(msg['msg_type'])
             print 'msg = %s' % str(msg)
+        sys.stdout.flush()
