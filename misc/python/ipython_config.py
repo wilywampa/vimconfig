42d4f01a14e604feb2c72b72ba147bf127bca43e
Jacob Niehus <jacob.niehus@gmail.com>
2015-04-25 17:32:24 -0700

    Add global IPython configuration file

---
+++ misc/python/ipython_config.py
@@ -0,0 +1,40 @@
+def configure(c):
+    """
+    Global IPython configuration.
+
+    >>> import imp
+    >>> import os
+    >>> imp.load_source('_ipython_config', os.path.join(
+    ...     os.environ['VIMCONFIG'], 'misc', 'python', 'ipython_config.py')
+    ... ).configure(c)
+
+    """
+    c.TerminalInteractiveShell.colors = 'Linux'
+    c.TerminalInteractiveShell.autocall = 1
+    c.TerminalInteractiveShell.confirm_exit = False
+    c.PromptManager.color_scheme = 'Linux'
+    c.IPCompleter.greedy = True
+    c.IPythonWidget.syntax_style = "solarizedlight"
+
+    def add(item):
+        if item not in c.InteractiveShellApp.exec_lines:
+            c.InteractiveShellApp.exec_lines.append(item)
+
+    lines = [
+        'from __future__ import division',
+        'from plottools import *',
+        'import plottools as pt',
+        'import matplotlib as mpl',
+        'import scipy.io as sio',
+        'import os',
+        'import re',
+        'import subprocess',
+        'import numpy as np',
+        'import scipy.constants as sc',
+        ('def setwidth(): os.environ["COLUMNS"] = '
+         'subprocess.check_output(["tput", "cols"])'),
+        'env = {k: v for k, v in os.environ.iteritems()}',
+        'exec("del who" if "who" in globals() else "pass")',
+        'from collections import Iterable',
+    ]
+    map(add, lines)
